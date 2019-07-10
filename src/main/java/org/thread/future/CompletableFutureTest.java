4c27d5d43e03d35d682b5149f2fe65ee3cedb673
vghy顾海洋 <hy_gu@ctrip.com>
2019-07-10 20:47:24 +0800

    update

--- src/main/java/org/thread/future/CompletableFutureTest.java
+++ src/main/java/org/thread/future/CompletableFutureTest.java
@@ -4,26 +4,225 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Supplier;
 
 /**
  * @author hy_gu on 2019/1/12
  **/
 public class CompletableFutureTest {
 
-  // https://blog.csdn.net/u011726984/article/details/79320004
+
   public static void main(String[] args) throws Exception {
+    // https://blog.csdn.net/u011726984/article/details/79320004
     // demo1();
     // demo2();
-    demo3();
     // demo3();
+    // demo4();
+
+    // https://mp.weixin.qq.com/s/UUFxxHrh6ON-jwskqIss7Q
+    // future1();
+    // future2();
+    // future3();
+    // future4();
+    // future5();
+    // future6();
+    // future7();
+    // future8();
+    // future9();
+    future10();
+  }
+
+  private static void future1() throws Exception {
+    CompletableFuture<String> future = new CompletableFuture<>();
+    new Thread(() -> {
+      // 2.1休眠3s，模拟任务计算
+      try {
+        Thread.sleep(3000);
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }
+
+      // 2.2设置计算结果到future
+      future.complete("hello,jiaduo");
+    }).start();
+
+    // 3.等待计算结果
+    System.out.println(future.get());
+  }
+
+  private static void future2() throws Exception {
+    CompletableFuture future = CompletableFuture.runAsync(() -> {
+      // 1.1.1休眠2s模拟任务计算
+      try {
+        Thread.sleep(2000);
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }
+      System.out.println("over");
+    });
+
+    // 1.2 同步等待异步任务执行结束
+    System.out.println(future.get());
+  }
+
+  private static void future3() throws Exception {
+    CompletableFuture future = CompletableFuture.supplyAsync(() -> {
+      // 1.1.1休眠2s模拟任务计算
+      try {
+        Thread.sleep(2000);
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }
+      return "good";
+    });
+
+    System.out.println(future.get());
+  }
+
+  private static void future4() throws Exception {
+    CompletableFuture<String> future = CompletableFuture.supplyAsync(() -> {
+      try {
+        Thread.sleep(2000);
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }
+      return "hello";
+    });
+
+    CompletableFuture future2 = future.thenRun(() -> {
+      try {
+        Thread.sleep(1000);
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
+      System.out.println(Thread.currentThread().getName());
+    });
+    // 3.同步等待twoFuture对应的任务完成，返回结果固定为null
+    System.out.println(future2.get());
+  }
+
+  private static void future5() throws Exception {
+    CompletableFuture<String> future = CompletableFuture.supplyAsync(() -> {
+      try {
+        Thread.sleep(2000);
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }
+      return "good";
+    });
+
+    CompletableFuture future2 = future.thenAccept((s) -> {
+      System.out.println(s);
+    });
+    // 3.同步等待twoFuture对应的任务完成，返回结果固定为null
+    System.out.println(future2.get());
+  }
+
+  private static void future6() throws Exception {
+    CompletableFuture<String> future = CompletableFuture.supplyAsync(() -> {
+      try {
+        Thread.sleep(2000);
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }
+      return "good good";
+    });
+
+    CompletableFuture future2 = future.thenApply(new Function<String, Object>() {
+      @Override
+      public String apply(String t) {
+        return t + " morning";
+      }
+    });
+
+    System.out.println(future2.get());
+  }
+
+  private static void future7() throws Exception {
+    CompletableFuture future = doSomethingOne("123").thenCompose(id -> doSomethingTwo(id));
+    System.out.println(future.get());
+  }
+
+  private static void future8() throws Exception {
+    CompletableFuture future = doSomethingOne("123").thenCombine(doSomethingTwo("456"), (one, two) -> {
+      System.out.println(one);
+      System.out.println(two);
+      return one + two;
+    });
+    System.out.println(future.get());
+  }
+
+  private static void future9() throws Exception {
+    // 1.创建future列表
+    List<CompletableFuture<String>> futureList = new ArrayList<>();
+    futureList.add(doSomethingOne("1"));
+    futureList.add(doSomethingOne("2"));
+    futureList.add(doSomethingOne("3"));
+    futureList.add(doSomethingOne("4"));
+
+    // 2.转换多个future为一个
+    CompletableFuture<Void> result =
+        CompletableFuture.allOf(futureList.toArray(new CompletableFuture[futureList.size()]));
+
+    // 3.等待所有future都完成
+    System.out.println(result.get());
+  }
+
+  private static void future10() throws Exception {
+    // 1.创建future列表
+    List<CompletableFuture<String>> futureList = new ArrayList<>();
+    futureList.add(doSomethingOne("1"));
+    futureList.add(doSomethingOne("2"));
+    futureList.add(doSomethingTwo("3"));
+
+    // 2.转换多个future为一个
+    CompletableFuture<Object> result =
+        CompletableFuture.anyOf(futureList.toArray(new CompletableFuture[futureList.size()]));
+
+    // 3.等待某一个future完成
+    System.out.println(result.get());
+  }
+
+  // 1.异步任务，返回future
+  private static CompletableFuture<String> doSomethingOne(String encodedCompanyId) {
+    // 1.1创建异步任务
+    return CompletableFuture.supplyAsync(() -> {
+      // 1.1.1休眠1s，模拟任务计算
+      try {
+        Thread.sleep(1000);
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }
+      return encodedCompanyId;
+    });
+  }
+
+  // 2.开启异步任务，返回future
+  private static CompletableFuture<String> doSomethingTwo(String companyId) {
+    return CompletableFuture.supplyAsync(() -> {
+      // 2.1,休眠3s，模拟计算
+      try {
+        Thread.sleep(2000);
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }
+      // 2.2 查询公司信息，转换为str，并返回
+      return companyId + ":alibaba";
+    });
+
   }
 
   private static void demo1() {
     CompletableFuture.supplyAsync(() -> "Hello").thenApply(s -> s + " world").thenApply(String::toUpperCase)
-        .thenCombine(CompletableFuture.completedFuture("Java"), (s1, s2) -> s1 + s2).thenAccept(System.out::println);
+        .thenCombine(CompletableFuture.completedFuture("Java"), (s1, s2) -> s1 + " " + s2)
+        .thenAccept(System.out::println);
 
   }
 
@@ -41,7 +240,6 @@ public class CompletableFutureTest {
     completableFuture.whenComplete((s, e) -> {
       System.out.println(s);
     });
-
   }
 
   private static void demo3() {
